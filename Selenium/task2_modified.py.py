7b4092e7fc6da5d47a72ccc750ac9b7c0aca6ff7
Prachi Shah <prachishah0567@gmail.com>
2022-04-20 11:21:37 +0530

    commit

--- Selenium/task2_modified.py.py
+++ Selenium/task2_modified.py.py
@@ -9,33 +9,35 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import time
 
 s=Service('/home/woc/Prachi/Training/Web_Scraping/Selenium/chromedriver')
 browser = webdriver.Chrome(service=s)
 url='https://www.blurtit.com'
 browser.get(url)
 
-topics = browser.find_element(by = By.CLASS_NAME, value = "hlink-topics")
-topics.click()
+# Topics
+first_section = browser.find_element(by = By.XPATH, value = '//*[ text() = \'Topics\' ]')
+first_section.click()
 
+#Science
+second_section = browser.find_element(by=By.XPATH, value='//*[ text() = \'Science\' ]')
+second_section.click()
 
-xpath_technology = "/html/body/div[3]/div[2]/ul/li[2]/div[2]/p"
-technology = browser.find_element(by=By.XPATH, value=xpath_technology)
-technology.click()
+#Computer SCience
+third_section = browser.find_element(by=By.XPATH, value='//*[ text() = \'Computer Science\' ]')
+third_section.click()
 
-xpath_sony = "/html/body/div[3]/div[2]/aside[2]/ul/li[6]/div[2]/p/a"
-sony = browser.find_element(by=By.XPATH, value=xpath_sony)
-sony.click()
+number_of_pages = 10
 
-for page in range(4):
+for page in range(number_of_pages):
     questions = browser.find_elements(by = By.XPATH,value = "//div[@class =\"feed-item-title clearfix\"]/a")
-    with open("MyFile.txt","a") as file1:
+    with open("Questions.txt","a") as question_file:
         for question in questions:
-            file1.write(question.text + "\n")
+            question_file.write(question.text + "\n")
             # print(questions[index].text)
-        if page == 3:
+        if page == number_of_pages - 1:
             break
-        next = browser.find_element(by = By.XPATH,value = "/html/body/div[3]/div[1]/div/div/ul/li[7]/a")
+        next = browser.find_element(by = By.XPATH,value = '//*[ text() = \'>\' ]')
         next.click()
-file1.close()
+question_file.close()
+
